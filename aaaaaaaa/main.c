b5a4a388f0fa05ef34004966bb008fdd6611b643
PEDRO IVO CANDEIA DE FIGUEIREDO <pfigueiredo@cs.unipe.edu.br>
2026-08-25 09:38:58 -0300

    Meu primeiro commit

---
+++ aaaaaaaa/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int idade;
+    float altura;
+
+    printf("Digite sua idade: ");
+    scanf("%d", &idade);
+
+    printf("Digite sua altura: ");
+    scanf("%f", &altura);
+
+    printf("\n Sua idade: %d", idade);
+    printf("\n Sua altura: %f", altura);
+
+    return 0;
+
+}
